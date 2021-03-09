522825059ec9502f36aad054b849852b070187fe
Ricardo Carvalho <rat.carvalho@gmail.com>
2021-03-09 15:21:04 -0300

    2021-03-09

--- set01/challenge06.py
+++ set01/challenge06.py
@@ -1,69 +1,24 @@
 from base64 import b64decode
-from typing import Dict, List, Iterable, Tuple
+from typing import List, Iterable, Tuple
 
 from lib import bytestohexstring, hd, transposebytes, wrapbytes
 from lib.chisquare import ChiSquare
+from lib.consts import INDICES
 
 
 class Answer(ChiSquare):
     def __init__(self, s: str, sizes: int, seq: Iterable[int]):
         self.bytes = b64decode(s)
-        self.indices: List[Tuple[int, int]] = [
-            (0, 1),
-            (0, 2),
-            (0, 3),
-            (1, 2),
-            (1, 3),
-            (2, 3),
-        ]
         self.minsizes = sizes
         self.seq = seq
 
-        self.ALPHABET: Dict[str, float] = {
-            "a": 0.08167,
-            "b": 0.01492,
-            "c": 0.02782,
-            "d": 0.04253,
-            "e": 0.12702,
-            "f": 0.02228,
-            "g": 0.02015,
-            "h": 0.06094,
-            "i": 0.06966,
-            "j": 0.00153,
-            "k": 0.00772,
-            "l": 0.04025,
-            "m": 0.02406,
-            "n": 0.06749,
-            "o": 0.07507,
-            "p": 0.01929,
-            "q": 0.00095,
-            "r": 0.05987,
-            "s": 0.06327,
-            "t": 0.09056,
-            "u": 0.02758,
-            "v": 0.00978,
-            "w": 0.02360,
-            "x": 0.00150,
-            "y": 0.01974,
-            "z": 0.00074,
-        }
-
-        self.l: List[int] = (
-            list(range(ord("a"), ord("z") + 1))
-            + list(range(ord("A"), ord("Z") + 1))
-            + list(range(ord(":"), ord("@") + 1))
-            + list(range(ord(" "), ord("/") + 1))
-            + list(range(ord("0"), ord("9") + 1))
-        )
-
         self.s = ""
-
         self.smallerkeys: List[int] = []
 
-    # TODO: Write unit tests for the 3 functions bellow
+    # TODO: Write unit tests for the 3 functions bellow. Problably will need to create a mock object
     def processkeysize(self, keysize: int) -> float:
         v = [self.bytes[i * keysize : (i + 1) * keysize] for i in range(4)]
-        r = [hd(v[i], v[k]) / keysize for i, k in self.indices]
+        r = [hd(v[i], v[k]) / keysize for i, k in INDICES]
         return sum(r) / len(r)
 
     def getkeysizes(self) -> List[int]:
--- set01/lib/chisquare.py
+++ set01/lib/chisquare.py
@@ -1,48 +1,12 @@
 import string
-from typing import Tuple, Dict, List
+from typing import Tuple
 
 from lib import xorsingleletter
+from lib.consts import ALPHABET, CHARS
 
 
 class ChiSquare:
     def __init__(self, s: str):
-        self.ALPHABET: Dict[str, float] = {
-            "a": 0.08167,
-            "b": 0.01492,
-            "c": 0.02782,
-            "d": 0.04253,
-            "e": 0.12702,
-            "f": 0.02228,
-            "g": 0.02015,
-            "h": 0.06094,
-            "i": 0.06966,
-            "j": 0.00153,
-            "k": 0.00772,
-            "l": 0.04025,
-            "m": 0.02406,
-            "n": 0.06749,
-            "o": 0.07507,
-            "p": 0.01929,
-            "q": 0.00095,
-            "r": 0.05987,
-            "s": 0.06327,
-            "t": 0.09056,
-            "u": 0.02758,
-            "v": 0.00978,
-            "w": 0.02360,
-            "x": 0.00150,
-            "y": 0.01974,
-            "z": 0.00074,
-        }
-
-        self.l: List[int] = (
-            list(range(ord("a"), ord("z") + 1))
-            + list(range(ord("A"), ord("Z") + 1))
-            + list(range(ord(":"), ord("@") + 1))
-            + list(range(ord(" "), ord("/") + 1))
-            + list(range(ord("0"), ord("9") + 1))
-        )
-
         self.s = s
 
     def chisquare(self, xored: str) -> float:
@@ -52,7 +16,7 @@ class ChiSquare:
                 if (not c.isascii()) or ord(c) > 128:
                     sum += float("inf")
                 elif c.isascii() and c.isalpha():
-                    e = self.ALPHABET[c]
+                    e = ALPHABET[c]
                     sum += ((1.0 - e) ** 2.0) / e
                 elif c.isascii() and c.isdecimal():
                     sum += 100.0
@@ -67,7 +31,7 @@ class ChiSquare:
     def analysis(self) -> Tuple[str, float, str]:
         bestfit = ("\0", float("inf"), "")
 
-        for c in self.l:
+        for c in CHARS:
             xored = xorsingleletter(self.s, c)
 
             bn = self.chisquare(xored.lower())
