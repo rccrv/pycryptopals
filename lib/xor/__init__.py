e0292ec502e1dfe8781c23df2d5fe2a4dbdbe31a
Ricardo Carvalho <rat.carvalho@gmail.com>
2021-03-23 17:09:36 -0300

    2021-03-23

--- lib/xor/__init__.py
+++ lib/xor/__init__.py
@@ -1,17 +1,19 @@
 from itertools import cycle
-from typing import Generic, TypeVar
+from typing import TypeVar
 
 
 T = TypeVar("T", str, bytes)
 
 
 def xor(a: T, b: T) -> T:
-    if isinstance(a, str):
+    if isinstance(a, str) and isinstance(b, str):
         return b"".join(
             [
                 b"%c" % (i[0] ^ i[1])
                 for i in zip(a.encode("utf-8"), cycle(b.encode("utf-8")))
             ]
         ).decode("utf-8")
-    else:
+    elif isinstance(a, bytes) and isinstance(b, bytes):
         return b"".join([b"%c" % (i[0] ^ i[1]) for i in zip(a, cycle(b))])
+    else:
+        raise TypeError("both arguments a and b should be of the same type")
