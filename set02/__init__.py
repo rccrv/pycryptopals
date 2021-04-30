ddfaff0de0fb1a69957b4ae332fac4fa43c44555
Ricardo Carvalho <rat.carvalho@gmail.com>
2021-04-30 18:15:05 -0300

    2021-04-30

--- set02/__init__.py
+++ set02/__init__.py
@@ -1,10 +1,13 @@
 from set02 import challenge09
 from set02 import challenge10
+from set02 import challenge11
 
 
 def challenges():
-    print("# cryptopals - set 02 #")
-    print("- challenge 09 -")
-    print(challenge09.answer())
-    print("- challenge 10 -")
-    print(challenge10.answer())
+    #print("# cryptopals - set 02 #")
+    #print("- challenge 09 -")
+    #print(challenge09.answer())
+    #print("- challenge 10 -")
+    #print(challenge10.answer())
+    print("- challenge 11 -")
+    print(challenge11.answer())
---
+++ set02/challenge11.py
@@ -0,0 +1,45 @@
+from enum import Enum
+from random import randbytes, randint, random
+
+from lib.aes import encryptaescbc, encryptaesecb
+from lib.data import Data
+
+class AESType(Enum):
+    CBC = 1
+    EBC = 2
+
+def generate_random_key() -> Data:
+    r = Data(randbytes(16))
+
+    return r
+
+def generate_ecb_or_cbc(b: Data, k: Data) -> Data:
+    d = Data(randbytes(randint(5, 10)) + b.data + randbytes(randint(5, 10)))
+    v = random()
+    r = encryptaescbc(b, k, Data(randbytes(16))) if v < 0.5 else encryptaesecb(b, k)
+    if v < 0.5:
+        print("CBC")
+    else:
+        print("EBC")
+    # r = encryptaescbc(b, k, Data(randbytes(16))) if random() < 0.5 else encryptaesecb(b, k)
+
+    return r
+
+def oracle(d: Data) -> AESType:
+    r = AESType.CBC
+
+    print(d.entropy())
+
+    return r
+
+def answer() -> str:
+    r = ""
+    for i in range(10):
+        b = Data(b"YELLOW SUBMARINE")
+        k = generate_random_key()
+        d = generate_ecb_or_cbc(b, k)
+        print(d)
+        oracle(d)
+        print("-----------------------------------------------")
+
+    return r
