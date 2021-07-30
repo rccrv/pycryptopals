cfe58f358f9faa0e227344532cf162749623ae10
Ricardo Carvalho <rat.carvalho@gmail.com>
2021-07-30 15:34:42 -0300

    2021-07-30

--- lib/aes/__init__.py
+++ lib/aes/__init__.py
@@ -1,7 +1,16 @@
+from enum import Enum
+from random import randbytes, randint, random
+from typing import Tuple
+
 from Crypto.Util.Padding import unpad
 from Crypto.Cipher import AES
 
 from lib.data import Data
+
+
+class AESType(Enum):
+    CBC = "CBC"
+    EBC = "EBC"
 
 
 def decrypt_aes_ecb_block(b: Data, k: Data) -> Data:
@@ -88,3 +97,27 @@ def pkcs7_pad(b: Data, n: int) -> Data:
     r = b + Data((b"%c" % bytes_to_add) * bytes_to_add)
 
     return r
+
+
+def generate_random_key() -> Data:
+    r = Data(randbytes(16))
+
+    return r
+
+
+def generate_ecb_or_cbc(b: Data, k: Data) -> Data:
+    d = Data(randbytes(randint(5, 10))) + b + Data(randbytes(randint(5, 10)))
+    v = random()
+    r = encrypt_aes_cbc(d, k, Data(randbytes(16))) if v < 0.5 else encrypt_aes_ecb(d, k)
+
+    return r
+
+
+def oracle_aes_ecb_or_cbc(d: Data) -> Tuple[AESType, int]:
+    r = ()  # type: ignore
+
+    h = d.wrap(16)
+    score = len(h) - len(set(h))
+    r = (AESType.EBC, score) if score > 0 else (AESType.CBC, score)  # type: ignore
+
+    return r  # type: ignore
--- set02/challenge11.py
+++ set02/challenge11.py
@@ -1,38 +1,5 @@
-from enum import Enum
-from random import randbytes, randint, random
-from typing import Tuple
-
-from lib.aes import encrypt_aes_cbc, encrypt_aes_ecb
+from lib.aes import generate_random_key, generate_ecb_or_cbc, oracle_aes_ecb_or_cbc
 from lib.data import Data
-
-
-class AESType(Enum):
-    CBC = "CBC"
-    EBC = "EBC"
-
-
-def generate_random_key() -> Data:
-    r = Data(randbytes(16))
-
-    return r
-
-
-def generate_ecb_or_cbc(b: Data, k: Data) -> Data:
-    d = Data(randbytes(randint(5, 10))) + b + Data(randbytes(randint(5, 10)))
-    v = random()
-    r = encrypt_aes_cbc(d, k, Data(randbytes(16))) if v < 0.5 else encrypt_aes_ecb(d, k)
-
-    return r
-
-
-def oracle(d: Data) -> Tuple[AESType, int]:
-    r = ()  # type: ignore
-
-    h = d.wrap(16)
-    score = len(h) - len(set(h))
-    r = (AESType.EBC, score) if score > 0 else (AESType.CBC, score)  # type: ignore
-
-    return r  # type: ignore
 
 
 def answer() -> str:
@@ -41,7 +8,7 @@ def answer() -> str:
         b = Data(f.read().strip())
         k = generate_random_key()
         d = generate_ecb_or_cbc(b, k)
-        o = oracle(d)
+        o = oracle_aes_ecb_or_cbc(d)
         r = f"Data was encrypted using {o[0].value} and has a score of {o[1]}"
 
     return r
